de47be1e0f7f57299d4d3b7e3345beab363553cb
root <root@ip-172-31-47-38.us-west-2.compute.internal>
2017-04-06 19:02:40 +0000

    edited mail logic

--- php/request.php
+++ php/request.php
@@ -4,19 +4,19 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $text = $_POST['message'];
 
+// Put message together in a variable 
 $msg = "Hello! \n\n $name has submitted the following request: \n\n '$text' \n\n Reply to: $email ";
 
 // use wordwrap() if lines are longer than 70 characters
 $msg = wordwrap($msg,70);
 
-// send email
-mail("rudy.marks@gmail.com","New Feature Request!",$msg);
-
+//If the text feild is empty then send to fail.html
 if(empty($text)){
   header('Location: /fail.html');
+//If it's not then send to thank you page and send email
 }else{
+   mail("rudy.marks@gmail.com","New Feature Request!",$msg);
    header('Location: /thanks.html');
-
 }
 
 
